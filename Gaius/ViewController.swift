a320e91664af63e3232b2b4150cd0878c31413c7
otavioalbucosta <otavio-albucosta@hotmail.com>
2022-10-11 09:06:56 -0300

    Initial Commit

---
+++ Gaius/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gaius
+//
+//  Created by Otávio Albuquerque on 11/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
